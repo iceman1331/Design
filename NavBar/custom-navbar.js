97d880583246026d4332a374ccb56e53bac0a73c
ruchirgupta121 <32531253+ruchirgupta121@users.noreply.github.com>
2020-09-17 18:39:53 +0530

    ...

--- NavBar/custom-navbar.js
+++ NavBar/custom-navbar.js
@@ -32,7 +32,7 @@ $(window).on("load resize", function () {
  * Listen to scroll to change header opacity class
  */
 function checkScroll(){
-    var startY = $('.navbar').height() * 10; //The point where the navbar changes in px
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
     if($(window).scrollTop() > startY){
         $('.navbar').addClass("scrolled");
@@ -49,6 +49,20 @@ if($('.navbar').length > 0){
 
 
 
+/**/
 
 
-/////
+// detect scroll top or down
+if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}
